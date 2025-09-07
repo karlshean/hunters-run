1a898e9903c0159c37df5197a317f9a8da51d8ae
karlshean <58302141+karlshean@users.noreply.github.com>
2025-09-07 13:41:45 -0700

    Update datasource.ts

--- apps/hr-api/src/shims/platform-db/src/datasource.ts
+++ apps/hr-api/src/shims/platform-db/src/datasource.ts
@@ -1,3 +1,2 @@
-// Matches imports like: import { dataSource } from '@platform/db/src/datasource';
 export const dataSource: any = {};
 export const getDataSource = async (): Promise<any> => dataSource;
